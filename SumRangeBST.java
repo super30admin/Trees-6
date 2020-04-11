111fff014ff8dd3d801e63bd4d7555a55b6eee19
unknown <sachinsharma3191@gmail.com>
2020-04-11 15:01:08 -0700

    Kindly review Trees-6

---
+++ SumRangeBST.java
@@ -0,0 +1,39 @@
+
+// Time Complexity : O(N) for All approaches
+// Space Complexity : O(N) for All approaches
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your Approach
+
+
+
+public class SumRangeBST {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        return (L <= root.val && root.val <= R ? root.val : 0) + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R);
+    }
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        if (root.val < L) return rangeSumBST(root.right, L, R); // left branch excluded.
+        if (root.val > R) return rangeSumBST(root.left, L, R); // right branch excluded.
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R); // count in both children.
+    }
+
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        Stack<TreeNode> stk = new Stack<>();
+        stk.push(root);
+        int sum = 0;
+        while (!stk.isEmpty()) {
+            TreeNode n = stk.pop();
+            if (n == null) { continue; }
+            if (n.val > L) { stk.push(n.left); } // left child is a possible candidate.
+            if (n.val < R) { stk.push(n.right); } // right child is a possible candidate.
+            if (L <= n.val && n.val <= R) { sum += n.val; }
+        }
+        return sum;
+    }
+}
