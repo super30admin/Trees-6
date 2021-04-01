862ed78759147b8f7e8c6737164867b6afcc261d
Anshul Atriek <atriekak@gmail.com>
2021-04-01 01:22:14 -0400

    Updated last problem with the BFS solution

--- Serialize and Deserialize Binary Tree.py
+++ Serialize and Deserialize Binary Tree.py
@@ -5,6 +5,69 @@
 #         self.left = None
 #         self.right = None
 
+from collections import deque
+
+class Codec:
+    #Approach: BFS
+    #Time Complexity: O(n)
+    #Space Complexity: O(n)
+    #where, n is the size of the tree
+    
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ''
+        
+        sb = []
+        de = deque([root])
+        
+        while de:
+            root = de.popleft()
+            if root:
+                sb.append(str(root.val))
+                
+                de.append(root.left)
+                de.append(root.right)
+            
+            else:
+                sb.append('None')
+                
+        return ','.join(sb)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        if not data:
+            return None
+        
+        data = data.split(',')
+        head = TreeNode(data[0])
+        
+        de = deque([head])
+        
+        idx = 0     #'used up' elements in data
+        while de:
+            root = de.popleft()
+            
+            if data[idx + 1] != 'None':
+                root.left = TreeNode(data[idx + 1])
+                de.append(root.left)
+            if data[idx + 2] != 'None':
+                root.right = TreeNode(data[idx + 2])
+                de.append(root.right)
+            idx += 2
+            
+        return head
+
+'''
 class Codec:
     #Approach: DFS
     #Time Complexity: O(n)
@@ -27,7 +90,6 @@ class Codec:
                 helper(root.right)
         
         helper(root)
-        print(','.join(sb))
         return ','.join(sb)
 
     def deserialize(self, data):
@@ -51,6 +113,7 @@ class Codec:
             return root
             
         return helper(data)
+'''
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
