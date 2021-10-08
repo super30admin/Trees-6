76626ab968f92d8dd0e43785feccd9411e2a5d78
Dheeraj Hiremath <hiremath.d@husky.neu.edu>
2021-10-07 21:37:29 -0400

    completed

---
+++ rangesumbst.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root is None:
+            return None
+        self.res=0
+        self.dfs(root,low,high)
+        return self.res
+    
+    def dfs(self,root,low,high):
+        if root == None:
+            return
+        if root.val >=low and root.val <= high:
+            self.res+=root.val
+        if root.val>=low:
+            self.dfs(root.left,low,high)
+        if root.val<=high:
+            self.dfs(root.right,low,high)
+    
+        
---
+++ serializedeserialize.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res=[]
+        queue=deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append("")
+        # print(res)
+        return ",".join(res)
+        
+
+    def deserialize(self, data):
+        if not data:
+            return
+        flat_bt = data.split(',')
+        ans = TreeNode(flat_bt[0])
+        queue = collections.deque([ans])
+        i = 1
+        # when you pop a node, its children will be at i and i+1
+        while queue:
+            node = queue.pop()
+            if i < len(flat_bt) and flat_bt[i]:
+                node.left = TreeNode(int(flat_bt[i]))
+                queue.appendleft(node.left)
+            i += 1
+            if i < len(flat_bt) and flat_bt[i]:
+                node.right = TreeNode(int(flat_bt[i]))
+                queue.appendleft(node.right)
+            i += 1
+        return ans
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
