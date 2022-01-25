b37cc1a995c46dcc1b7282e29c8adec20e0c9265
Adhish Shrivastava <adhish70@gmail.com>
2022-01-25 12:31:51 -0500

    Problem 174 done

--- Problem-174 Range Sum of BST.py
+++ Problem-174 Range Sum of BST.py
@@ -1,10 +1,14 @@
 # 938. Range Sum of BST
 # https://leetcode.com/problems/range-sum-of-bst/
 
-# Logic: 
+# Logic: We check the node's value with the range. As it is a BST, 
+# the left side would be strictly less than and right side will be 
+# strictly greater than the parent. If the parent is greater than 
+# low only then we check the left subtree. Similarly if the parent 
+# is less than high, only then we check the right subtree.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,6 +17,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        pass
+    def _helper(self, node, low, high):
+        if not node:
+            return 0
         
+        s = 0
+        if node.val >= low:
+            s += self._helper(node.left, low, high)
+        
+        if node.val <= high:
+            s += self._helper(node.right, low, high)
+        
+        if low <= node.val <= high:
+            s += node.val
+        
+        return s
+    
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        return self._helper(root, low, high)
