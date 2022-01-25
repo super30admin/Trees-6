82bfd319b66024abf64d5a2ff27e68e430c69382
Adhish Shrivastava <adhish70@gmail.com>
2022-01-25 12:29:18 -0500

    Initial Commit

---
+++ Problem-174 Range Sum of BST.py
@@ -0,0 +1,18 @@
+# 938. Range Sum of BST
+# https://leetcode.com/problems/range-sum-of-bst/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        pass
+        
---
+++ Problem-175 Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,39 @@
+# 297. Serialize and Deserialize Binary Tree
+# https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        pass
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        pass
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
